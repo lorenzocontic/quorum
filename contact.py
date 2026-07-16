b852c3ba8d09a39887bbcf6a7ab4e29413623b81
lorenzocontic <lorenzocontic@proton.me>
2026-07-16 19:18:11 +0300

    Create contact.py

---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+
+    def __init__(
+        self,
+        name,
+        company,
+        phone,
+        email
+    ):
+
+        self.name = name
+        self.company = company
+        self.phone = phone
+        self.email = email
